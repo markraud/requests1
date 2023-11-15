9acee9208f4dc33aac858ae7b1e69c4e27086b1d
Mark Raudenbush <markraud@hotmail.com>
2023-11-15 11:38:42 -0700

    working with params

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ import requests
 import json
 
 baseUrl = "http://api.exchangeratesapi.io/v1/latest?access_key=b5af89a7098ae2d487a95f40fb24007e"
-
-response = requests.get(baseUrl)
+paramURL = baseUrl + "&symbols=USD,GBP"
+response = requests.get(paramURL)
 
 print(response.status_code)
-print(response.json())
+# print(json.dumps(response.json(), indent=4, sort_keys=True))
+# print(response.json().keys())
+# print(paramURL)
+
+data = response.json()
+
+print(f'here is the base = {data["base"]}')
+print(f'here is the base = {data["date"]}')
+print(f'here is the base = {data["rates"]}')
