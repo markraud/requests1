57287c316c59eeb7c2c56eecc2482de0b54f71f3
Mark Raudenbush <markraud@hotmail.com>
2023-11-15 13:06:03 -0700

    historical rates endpoint

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import requests
 import json
 
-# histDate = '/2016-01-26'
-
+dateVal = '2016-01-26'
 accessKey = 'b5af89a7098ae2d487a95f40fb24007e'
 baseUrl = f"http://api.exchangeratesapi.io/v1/"
-# paramURL = f"{baseUrl}{dateVal}?access_key={accessKey}"
-timePeriod = f"{baseUrl}timeseries?access_key={accessKey}&start_date=2017-04-26&end_date=2018-04-26&base=EUR&symbols=GBP"
-response = requests.get(timePeriod)
+paramURL = f"{baseUrl}{dateVal}?access_key={accessKey}"
+response = requests.get(paramURL)
 data = response.json()
-print(response.status_code)
-print(json.dumps(data, indent=4, sort_keys=True))
+# print(response.status_code)
+# print(json.dumps(data, indent=4, sort_keys=True))
 
 
-# eurToGbp = data["rates"]["GBP"]
-# print(f'1 GBP in EUR = {eurToGbp}')
+eurToGbp = data["rates"]["GBP"]
+print(f'1 GBP in EUR = {eurToGbp}')
