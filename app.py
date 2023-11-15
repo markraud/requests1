466c6b0b29f41f9b262df03602afc1a060a80634
Mark Raudenbush <markraud@hotmail.com>
2023-11-15 11:54:33 -0700

    base currency and rate

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ import requests
 import json
 
 baseUrl = "http://api.exchangeratesapi.io/v1/latest?access_key=b5af89a7098ae2d487a95f40fb24007e"
-paramURL = baseUrl + "&symbols=USD,GBP"
-response = requests.get(paramURL)
+paramURL = baseUrl + "&symbols=GBP" + "&" + "base=EUR"  #had to use EUR as base as all other currencies are only available for paid subscription
+data = requests.get(paramURL).json()
 
-print(response.status_code)
+eurToGbp = data["rates"]["GBP"]
+print(f'1 GBP in EUR = {eurToGbp}')
+
+
+
 # print(json.dumps(response.json(), indent=4, sort_keys=True))
 # print(response.json().keys())
 # print(paramURL)
 
-data = response.json()
-
-print(f'here is the base = {data["base"]}')
-print(f'here is the base = {data["date"]}')
-print(f'here is the base = {data["rates"]}')
+# print(f'here is the base = {data["base"]}')
+# print(f'here is the base = {data["date"]}')
+# print(f'here is the base = {data["rates"]}')
